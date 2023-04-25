3d47c9e681f3a6352a90c33160d9502c03f662a8
Sofiia Shevchenko <118025665+shsv25@users.noreply.github.com>
2023-04-26 02:40:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// 13 варіант
+using System;
+
+
+namespace Project_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введiть речення: ");
+            string sentence = Console.ReadLine();
+
+            Console.Write("Введiть лiтеру: ");
+            char letter = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            string[] words = sentence.Split(' ');  // розділяємо речення на слова
+
+            Console.WriteLine("Слова, що починаються з лiтери '{0}':", letter);
+            foreach (string word in words)
+            {
+                if (word.StartsWith(letter.ToString(), StringComparison.CurrentCultureIgnoreCase))
+                {
+                    Console.WriteLine(word);
+                }
+            }
+        }
+    }
+}
